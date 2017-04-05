2fd75c78a7a0a58e77b50598ee1150a28100a7fa
Susan Yuen <yuens2@mcmaster.ca>
2017-04-05 15:23:01 -0400

    Added HotelController class. Moved scripts and scenes into Script/Scene folders.

---
+++ Paradise Towers/Assets/Scripts/HotelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HotelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /** MAIN MENU FUNCTIONS **/
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
