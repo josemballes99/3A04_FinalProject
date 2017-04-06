6d776ad21b63c76cc3b6d0e4a4d7ec8ad5b9d596
Susan Yuen <yuens2@mcmaster.ca>
2017-04-06 17:15:56 -0400

    Added upgrade floor button

---
+++ Paradise Towers/Assets/Scripts/Supporting Scripts/ToggleButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleButton : MonoBehaviour {
+
+    public Button toggleButton;
+    public GameObject UI;
+
+	// Use this for initialization
+	void Start () {
+        toggleButton.onClick.AddListener(() => togglePanel());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void togglePanel()
+    {
+        bool active = UI.activeSelf;
+        UI.SetActive(!active);
+    }
+}
